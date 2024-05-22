2531fe46f6dbfd9dfa68867a61879bf89d3fa381
Andrew Mellen <andrew_mellen@icloud.com>
2024-05-22 19:08:08 -0400

    update login endpoint

--- src/components/navbar/src/login/LoginButton.tsx
+++ src/components/navbar/src/login/LoginButton.tsx
@@ -2,7 +2,7 @@ import { Button } from "flowbite-react";
 
 export function LoginButton() {
   return (
-    <Button href="http://localhost:3000/auth/login" color="yellow">
+    <Button href="http://localhost:3000/api/v1/auth/login" color="yellow">
       Login
     </Button>
   );
