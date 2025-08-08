9dcd7800d9c8e9555010602b1f17e75297e1cd30
Andrew Mellen <andrew_mellen@icloud.com>
2025-08-08 13:25:30 -0400

    start of move to shadcn components

---
+++ src/components/customized/navigation-menu/navigation-menu-05.tsx
@@ -0,0 +1,48 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+import { cn } from "@/lib/utils";
+import { Link, useLocation } from "react-router-dom";
+
+export interface NavbarMenuItem {
+  title: string
+  href: string
+}
+
+export interface NavigationMenuWithActiveItemProps {
+  items: NavbarMenuItem[]
+}
+
+export default function NavigationMenuWithActiveItem({ items }: NavigationMenuWithActiveItemProps) {
+  const location = useLocation();
+
+  return (
+    <NavigationMenu>
+      <NavigationMenuList className="space-x-8">
+        {items.map((item) => (
+          <NavigationMenuItem key={item.title}>
+            <NavigationMenuLink
+              className={cn(
+                "relative group inline-flex h-9 w-max items-center justify-center px-0.5 py-2 text-sm font-medium",
+                "before:absolute before:bottom-0 before:inset-x-0 before:h-[2px] before:bg-primary before:scale-x-0 before:transition-transform",
+                "hover:before:scale-x-100 hover:text-accent-foreground",
+                "focus:before:scale-x-100 focus:text-accent-foreground focus:outline-none",
+                "disabled:pointer-events-none disabled:opacity-50",
+                "data-[active]:before:scale-x-100 data-[state=open]:before:scale-x-100"
+              )}
+              asChild
+              active={location.pathname === item.href}
+            >
+              <Link to={item.href}>
+                {item.title}
+              </Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        ))}
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
--- src/components/navbar/src/Navbar.tsx
+++ src/components/navbar/src/Navbar.tsx
@@ -10,8 +10,33 @@ import { Link, useLocation } from "react-router-dom";
 
 import { Login } from "./login";
 import { AuthContext, isAdmin } from "@/state/auth";
+import NavigationMenuWithActiveItem, { type NavbarMenuItem } from "@/components/customized/navigation-menu/navigation-menu-05";
+
 
 export function Navbar() {
+  const { authState } = useContext(AuthContext);
+
+  const navBarItems: NavbarMenuItem[] = [
+    { title: "Home", href: "/" },
+    ...(isAdmin(authState) ? [
+      { title: "Admin", href: "/admin" }
+    ] : [])
+  ];
+
+  return (
+    <div className="flex justify-between items-center border-b-2 border-yellow-300 py-2.5 px-2 sm:px-4 bg-emerald-900 shadow-lg">
+      <div>
+          Boston Combat Archery
+      </div>
+      <NavigationMenuWithActiveItem items={navBarItems} />
+      <div className="flex md:order-2 space-x-2">
+        <Login />
+      </div>
+    </div>
+  );
+}
+
+export function OldNavbar() {
   const customTheme: CustomFlowbiteTheme["navbar"] = {
     root: {
       base: "px-2 py-2.5 sm:px-4 border-b-2 border-yellow-300 bg-emerald-900 shadow-lg",
